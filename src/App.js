1983a0abc4f7884f664189e881ec398a98516886
aar-atiq <aar.msdeveloper01@gmail.com>
2021-03-04 05:38:32 +0600

    created navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './Components/Header/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
   return (
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,30 +1,39 @@
 import React from 'react';
 import './Header.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faBookOpen, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { Button, Dropdown, Navbar, } from 'react-bootstrap';
 
 const Header = () => {
     return (
-        <div>
+        <Navbar>
+            <div className="navBrand">
+                <i className="icon"><FontAwesomeIcon icon={faBookOpen} /> </i>
+                <h1>Book<span>land</span></h1>
+            </div>
             <div class="wrapper">
                 <div class="search_box">
-                    <div class="dropdown">
-                        <div class="default_option">Menu</div>
-                        <ul>
-                            <li>Student</li>
-                            <li>Book</li>
-                            <li>About Us</li>
-                        </ul>
-                    </div>
+                    <Dropdown className="menus" id="basic-nav-dropdown">
+                        <Dropdown.Toggle variant="light" id="dropdown-basic">
+                            Menus
+                         </Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            <Dropdown.Item href="/book">Books</Dropdown.Item>
+                            <Dropdown.Item href="/literature">Literature</Dropdown.Item>
+                            <Dropdown.Item href="/author">Author</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
                     <div class="search_field">
                         <input type="text" class="input" placeholder="Search books here" />
                         <i class="fas"><FontAwesomeIcon icon={faSearch} /></i>
                     </div>
                 </div>
-                <button>Login</button>
-                <button>Sign Up</button>
+                <div className="main-btn">
+                    <Button id="navLoginBtn" variant="light">Login</Button>{' '}
+                    <Button id="navSignUpBtn" variant="warning">Sign Up</Button>{' '}
+                </div>
             </div>
-        </div>
+        </Navbar>
     );
 };
 
